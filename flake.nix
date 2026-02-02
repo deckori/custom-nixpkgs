fc127022fbf6acbd77301592b041b4d3b728de8d
deckori <deckori9@gmail.com>
2026-02-02 12:15:36 +0300

    2048: rm

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
       packages.${system} = {
         alias-finder-nu = pkgs.callPackage ./pkgs/alias-finder-nu { };
         pano-scrobbler = pkgs.callPackage ./pkgs/pano-scrobbler { };
-        2048 = pkgs.callPackage ./pkgs/2048 { };
         pix2text = pkgs.callPackage ./pkgs/pix2text { };
         program-chooser = pkgs.callPackage ./pkgs/program-chooser { };
         taskopen = pkgs.callPackage ./pkgs/taskopen { };

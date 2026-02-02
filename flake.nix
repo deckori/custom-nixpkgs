0df8be0f4a571e667ffcaa06ea44dd7e41187c5f
deckori <deckori9@gmail.com>
2026-02-02 12:15:33 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.alias-finder-nu = pkgs.callPackage ./alias-finder-nu/shell.nix { };
+    };
+}

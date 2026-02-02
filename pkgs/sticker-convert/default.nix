d7a69f6a7cc814fd82297de61e6ebf443ff00a80
deckori <deckori9@gmail.com>
2026-02-02 12:40:58 +0300

    sticker-convert: fix

--- pkgs/sticker-convert/default.nix
+++ pkgs/sticker-convert/default.nix
@@ -6,24 +6,44 @@
 
 let
   version = "2.18.2.0";
-  pname = "quba";
+  pname = "sticker-convert";
 
   src = fetchurl {
     url = "https://github.com/laggykiller/sticker-convert/releases/download/v${version}/sticker-convert-x86_64.AppImage";
-    hash = lib.fakeHash;
+    hash = "sha256-12WLGNY+F29Lask6Zz7nMulZ7PYlWOYh5hLlz4Z48yA=";
   };
 
-  appimageContents = appimageTools.extractType1 { inherit pname src; };
+  appimageContents = appimageTools.extractType2 { inherit pname src; };
+
+  icon = fetchurl {
+    url = "https://raw.githubusercontent.com/laggykiller/sticker-convert/refs/tags/v${version}/src/sticker_convert/resources/appicon.png";
+    hash = "sha256-6XBEQz7PnerqS43aRkwpWolFG4WvKMuQ+st1ly+/JPg=";
+  };
 in
 appimageTools.wrapType2 rec {
   inherit pname version src;
 
   extraInstallCommands = ''
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace-fail 'Exec=AppRun' 'Exec=${meta.mainProgram}'
+      # icon
+      mkdir -p $out/share/icons/hicolor/256x256/apps
+      install -Dm644 ${icon} \
+        $out/share/icons/hicolor/256x256/apps/sticker-convert.png
+
+      # desktop entry
+      mkdir -p $out/share/applications
+      cat > $out/share/applications/sticker-convert.desktop <<EOF
+    [Desktop Entry]
+    Name=sticker-convert
+    Exec=sticker-convert
+    Icon=sticker-convert
+    Type=Application
+    Categories=Utility;
+    Comment=Convert (animated) stickers between messaging platforms
+    EOF
   '';
 
   meta = {
+    mainProgram = "sticker-convert";
     description = "Convert (animated) stickers to/from WhatsApp, Telegram, Signal, Line, Kakao, Naver Band, OGQ, Viber, Discord, iMessage. Written in Python.";
     homepage = "https://github.com/laggykiller/sticker-convert/";
     downloadPage = "https://github.com/laggykiller/sticker-convert/releases";
